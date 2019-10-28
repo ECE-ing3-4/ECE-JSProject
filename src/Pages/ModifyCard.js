f44aa4a4efa222035984fb470eedbce1013466d9
NeilSCGH <neil.segard@gmail.com>
2019-10-28 10:06:16 +0100

    n

---
+++ src/Pages/ModifyCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Form from '../Forms/Form.js';
+
+function printfcard(listCards, usrID) {
+    let str = listCards.map((card) => {
+        if (card.user_id == usrID) {
+            return (
+                <p>
+                    Your {card.brand} card (XXXX-XXXX-XXXX-{card.last_4}) expire at {card.expired_at}.
+            </p>
+            );
+        }
+    });
+    if (str == "") {
+        return ("No Cards");
+    }
+    return (str);
+}
+
+export default function modifycard(obj, listCards) {
+    return (
+        <div>
+            <Route exact path="/modifycard" component={() =>
+                <>
+                    <p>Modify card</p>
+                    {obj.connected() || obj.acceptNotLoginFnc() ?
+                        <>
+                            {printfcard(listCards, obj.getCurrentID())}
+                            <Form inputNames={["last_4", "newBrand"]} inputTexts={["Last 4 digits of the card", "New brand"]} buttonText={"Change the brand"} onSend={obj.handleBrandChangeForm} />
+                        </>
+                        : "Connection requise"}
+                </>
+            } />
+        </div>
+    )
+
+
+
+
+}
